b7072abff02532db778d7ff2d8ad5c9fb7ebb4bd
rozeroze <rosettastone1886@gmail.com>
2018-08-30 12:56:12 +0900

    Add files via upload

---
+++ docs/ocean.js
@@ -0,0 +1,19 @@
+﻿$(function () {
+    $('#storage-change-btn').on('click', function () {
+        var chk = $('input[name=icon]:checked').val();
+        var path = window.location.origin + window.location.pathname;
+        if (chk == undefined || chk == 'cookie') {
+            // local-storage <Web Storage>
+            window.location = path + '?storage=local';
+        } else if (chk == 'github') {
+            // TODO: github
+            var url = $('input#github-url').val();
+            window.location = path + '?storage=' + url;
+        } else if (chk == 'dropbox') {
+            // TODO: dropbox
+        } else {
+            // error
+        }
+    });
+})
+
